813efb13f051f130c05be128eed5c2d91b4db8c3
Сидорович Никита Сергеевич <sidorovich_ns@accept.nsk.su>
2025-09-19 12:40:24 +0700

    прокинул в импорт EmailRecepient

--- atk/notifications/__init__.py
+++ atk/notifications/__init__.py
@@ -1,6 +1,6 @@
 """Пакет для работы с уведомлениями."""
 
-from .email.models import EmailAttachment, EmailMessage
+from .email.models import EmailAttachment, EmailMessage, EmailRecipient
 from .email.sender import EmailSender
 
-__all__ = ["EmailMessage", "EmailAttachment", "EmailSender"]
+__all__ = ["EmailMessage", "EmailAttachment", "EmailSender", "EmailRecipient"]
